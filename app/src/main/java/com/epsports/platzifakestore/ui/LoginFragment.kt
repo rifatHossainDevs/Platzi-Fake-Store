ecbf5d47979367a4b5281c0280779dde3a27aa57
Rifat Hossain <mdrifathossain472@gmail.com>
2025-05-15 03:28:09 +0600

    initial commit

--- app/src/main/java/com/epsports/platzifakestore/ui/LoginFragment.kt
+++ app/src/main/java/com/epsports/platzifakestore/ui/LoginFragment.kt
@@ -2,17 +2,19 @@ package com.epsports.platzifakestore.ui
 
 import android.annotation.SuppressLint
 import android.content.Context.MODE_PRIVATE
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.epsports.platzifakestore.databinding.FragmentLoginBinding
 import com.epsports.platzifakestore.model.RequestLogin
 import com.epsports.platzifakestore.viewModel.AuthViewModel
-
+import androidx.core.content.edit
 
 class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
@@ -25,14 +27,22 @@ class LoginFragment : Fragment() {
     ): View? {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
         sharedPreference = requireContext().getSharedPreferences("saveNote", MODE_PRIVATE)
+        binding.btnLogin.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
 
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                val requestLogin = RequestLogin(email, password)
 
-        val email = binding.etEmail.text.toString()
-        val password = binding.etPassword.text.toString()
-
-        val requestLogin = RequestLogin(email, password)
-
-        viewModel.authLogin(requestLogin)
+                viewModel.authLogin(requestLogin)
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Email or Password cannot be empty",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
 
         observerListener()
 
@@ -41,12 +51,28 @@ class LoginFragment : Fragment() {
 
     @SuppressLint("CommitPrefEdits")
     private fun observerListener() {
-        viewModel.login.observe (viewLifecycleOwner){responseDTO->
-            val sharedEdit = sharedPreference.edit()
+        viewModel.login.observe(viewLifecycleOwner) { responseDTO ->
+            if (responseDTO != null) {
+                sharedPreference.edit {
 
-            sharedEdit.putString("accessToken", responseDTO.accessToken)
-            sharedEdit.putString("refreshToken", responseDTO.refreshToken)
-            sharedEdit.apply()
+                    putString("accessToken", responseDTO.accessToken)
+                    putString("refreshToken", responseDTO.refreshToken)
+                }
+
+                Toast.makeText(requireContext(), "Login Successful", Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(requireContext(), MainActivity::class.java)
+                startActivity(intent)
+
+                requireActivity().finish()
+
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Login Failed. Check credentials.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 }
