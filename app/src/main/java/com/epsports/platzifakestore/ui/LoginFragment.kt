c9d84991a5f7420d7a27fe48922541f93d784d62
Rifat Hossain <mdrifathossain472@gmail.com>
2025-05-16 01:34:42 +0600

    login Fragment

--- app/src/main/java/com/epsports/platzifakestore/ui/LoginFragment.kt
+++ app/src/main/java/com/epsports/platzifakestore/ui/LoginFragment.kt
@@ -15,7 +15,9 @@ import com.epsports.platzifakestore.databinding.FragmentLoginBinding
 import com.epsports.platzifakestore.model.RequestLogin
 import com.epsports.platzifakestore.viewModel.AuthViewModel
 import androidx.core.content.edit
+import androidx.navigation.fragment.findNavController
 import com.epsports.platzifakestore.Nodes
+import com.epsports.platzifakestore.R
 
 class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
@@ -28,6 +30,15 @@ class LoginFragment : Fragment() {
     ): View? {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
         sharedPreference = requireContext().getSharedPreferences("saveNote", MODE_PRIVATE)
+
+        allClickListener()
+
+        observerListener()
+
+        return binding.root
+    }
+
+    private fun allClickListener() {
         binding.btnLogin.setOnClickListener {
             val email = binding.etEmail.text.toString()
             val password = binding.etPassword.text.toString()
@@ -45,9 +56,9 @@ class LoginFragment : Fragment() {
             }
         }
 
-        observerListener()
-
-        return binding.root
+        binding.btnRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registrationFragment)
+        }
     }
 
     @SuppressLint("CommitPrefEdits")
@@ -55,25 +66,20 @@ class LoginFragment : Fragment() {
         viewModel.login.observe(viewLifecycleOwner) { responseDTO ->
             if (responseDTO != null) {
                 sharedPreference.edit {
-
                     putString(Nodes.ACCESS_TOKEN, responseDTO.accessToken)
                     putString(Nodes.REFRESH_TOKEN, responseDTO.refreshToken)
                 }
 
                 Toast.makeText(requireContext(), "Login Successful", Toast.LENGTH_SHORT).show()
-
                 val intent = Intent(requireContext(), MainActivity::class.java)
                 startActivity(intent)
-
                 requireActivity().finish()
-
-            } else {
-                Toast.makeText(
-                    requireContext(),
-                    "Login Failed. Check credentials.",
-                    Toast.LENGTH_SHORT
-                ).show()
             }
         }
+
+        viewModel.loginError.observe(viewLifecycleOwner) { error ->
+            Toast.makeText(requireContext(), error, Toast.LENGTH_SHORT).show()
+        }
     }
+
 }
